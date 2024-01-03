2a35a175bbbe4e72880424a050d74eeea43d7702
HARDPC <HARDPC@WOTUM>
2024-01-03 14:00:09 +0100

    Add project files.

---
+++ Lekcja 7 zadanie 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Rok_Przestepny;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Podaj rok do sprawdzenia:");
+        int rok;
+
+        if (int.TryParse(Console.ReadLine(), out rok))
+        {
+            if ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0))
+            {
+                Console.WriteLine($"{rok} jest rokiem przestępnym");
+            }
+            else
+            {
+                Console.WriteLine($"{rok} nie jest rokiem przestępnym");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Podano nieprawidłową wartość roczną");
+        }
+
+    }
+
+}
